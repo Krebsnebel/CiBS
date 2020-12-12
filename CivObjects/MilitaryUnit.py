ab8847e1745e9306648d19326d25949a6549c34b
Kyrill Krajoski <kyrill.krajoski@gmail.com>
2020-12-12 14:45:42 +0100

    add class Military Unit
    with it all properties of military unit can be defined
    * unit type
    * unit strength
    * visibility
    * image pointer of front and back

---
+++ CivObjects/MilitaryUnit.py
@@ -0,0 +1,49 @@
+from CivEnums.EArmyStrength import EArmyStrength
+from CivEnums.EUnitType import EUnitType
+from CivEnums.EVisibility import EVisibility
+from Drawing import ImageHandler
+from Drawing.DrawCivObjects import DrawCivObjects
+
+
+"""
+with it all properties of military unit can be defined
+* unit type
+* unit strength
+* visibility
+* image pointer of front and back
+"""
+
+
+class MilitaryUnit:
+
+    def __init__(self, utype, strength):
+        self.uType = utype
+        self.unitStrength = strength
+        self.visibility = EVisibility.FOR_NOBODY
+        self.imgFront = ImageHandler.getImageOfMilitaryUnit(utype, strength, True)
+        self.imgBack = ImageHandler.getImageOfMilitaryUnit(utype, strength, False)
+
+    def getFightValue(self, rank):
+        return self.unitStrength.getFightValue(rank)
+
+    def changeVisibility(self, vis):
+        self.visibility = vis
+
+    def getCosts(self, rank):
+        if self.utype == EUnitType.AIR_FORCE:
+            return 12
+        elif rank == EArmyStrength.FORTH_LEVEL:
+            return 11
+        elif rank == EArmyStrength.THIRD_LEVEL:
+            return 9
+        elif rank == EArmyStrength.SECOND_LEVEL:
+            return 7
+        else:
+            return 5
+
+    def draw(self, window, rotation, pos, resize):
+        if self.visibility == EVisibility.FOR_NOBODY:
+            img = self.imgBack
+        else:
+            img = self.imgFront
+        DrawCivObjects.drawImage(img, window, rotation, pos, resize, 1)
