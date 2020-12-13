784ccf677e31b1ce259dce6f61f2d8921de03b51
Kyrill Krajoski <kyrill.krajoski@gmail.com>
2020-12-13 13:35:51 +0100

    add class Trade
    this class handles the trade process (phase 2)

---
+++ Game/Trade.py
@@ -0,0 +1,21 @@
+from Game.EGameStep import EGameStep
+
+
+"""
+this class handles the trade process (phase 2)
+"""
+
+
+class Trade:
+
+    def __init__(self, gameStep):
+        self.gameStep = gameStep
+
+    def execute(self):
+        if self.isInStep(EGameStep.COUNT_TRADING_POINTS):
+            pass
+        elif self.isInStep(EGameStep.SWITCH_CIVILIZATION):
+            pass
+
+    def isInStep(self, step):
+        return self.gameStep.get() == step
