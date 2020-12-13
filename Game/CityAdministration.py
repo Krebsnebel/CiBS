128757e979b470b811448b1e963935c899ba2c9d
Kyrill Krajoski <kyrill.krajoski@gmail.com>
2020-12-13 13:37:51 +0100

    add class CityAdministration
    this class handles the city administration (phase 3)

---
+++ Game/CityAdministration.py
@@ -0,0 +1,27 @@
+from Game.EGameStep import EGameStep
+
+
+"""
+this class handles the city administration (phase 3)
+"""
+
+
+class CityAdministration:
+
+    def __init__(self, gameStep):
+        self.gameStep = gameStep
+
+    def execute(self):
+        if self.isInStep(EGameStep.SET_STRUCTURE):
+            pass
+        elif self.isInStep(EGameStep.SET_FIGURE):
+            pass
+        elif self.isInStep(EGameStep.ARM_MILITARY_UNIT):
+            pass
+        elif self.isInStep(EGameStep.MINING_RESOURCE):
+            pass
+        elif self.isInStep(EGameStep.SWITCH_CIVILIZATION):
+            pass
+
+    def isInStep(self, step):
+        return self.gameStep.get() == step
