cce3d4434d7c48f4904ef8f6aa330a47b9556c0d
Kyrill Krajoski <kyrill.krajoski@gmail.com>
2020-12-13 13:44:18 +0100

    add class ResearchAndDevelopment
    this class handles the research and development process (phase 5)

---
+++ Game/ResearchAndDevelopment.py
@@ -0,0 +1,25 @@
+from Game.EGameStep import EGameStep
+
+
+"""
+this class handles the research and development process (phase 5)
+"""
+
+
+class ResearchAndDevelopment:
+
+    def __init__(self, gameStep):
+        self.gameStep = gameStep
+
+    def execute(self):
+        if self.isInStep(EGameStep.CHOOSE_RESEARCH):
+            pass
+        elif self.isInStep(EGameStep.SWIPE_RESEARCH):
+            pass
+        elif self.isInStep(EGameStep.CONFIRM_RESEARCH):
+            pass
+        elif self.isInStep(EGameStep.SWITCH_CIVILIZATION):
+            pass
+
+    def isInStep(self, step):
+        return self.gameStep.get() == step
