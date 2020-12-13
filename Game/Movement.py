71ef80b9c4380ffb0b02391462c2b8224b0d7ab7
Kyrill Krajoski <kyrill.krajoski@gmail.com>
2020-12-13 13:43:37 +0100

    add class Movement
    this class handles the movement process (phase 4)

---
+++ Game/Movement.py
@@ -0,0 +1,25 @@
+from Game.EGameStep import EGameStep
+
+
+"""
+this class handles the movement process (phase 4)
+"""
+
+
+class Movement:
+
+    def __init__(self, gameStep):
+        self.gameStep = gameStep
+
+    def execute(self):
+        if self.isInStep(EGameStep.CHOOSE_SQUARE):
+            pass
+        elif self.isInStep(EGameStep.CHOOSE_FIGURE):
+            pass
+        elif self.isInStep(EGameStep.SET_DESTINATION_SQUARE):
+            pass
+        elif self.isInStep(EGameStep.SWITCH_CIVILIZATION):
+            pass
+
+    def isInStep(self, step):
+        return self.gameStep.get() == step
